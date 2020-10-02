c8e7012f17b91e3905204cfd940ddc51e97da481
Shubham Gupta <61947842+starkex@users.noreply.github.com>
2020-10-02 19:57:13 +0530

    no

--- dogatime/rj/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import '../node_modules/normalize.css/normalize.css';
-import './styles/styles.scss';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
